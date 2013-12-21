0af5232a85e8da5966e9df91dceae132a4fc52cb
SY Quek <quekshuy@gmail.com>
2013-12-21 17:44:26 +0800

    Initial date done.

---
+++ Gruntfile.js
@@ -0,0 +1,132 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('grunt-aws.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    s3: {
+        options: {
+            key: '<%= aws.key %>',
+            secret: '<%= aws.secret %>',
+            bucket: '<%= aws.bucket %>',
+            access: 'public-read',
+            headers: {
+                 // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+                 "Cache-Control": "max-age=630720000, public",
+                 "Expires": new Date(Date.now() + 63072000000).toUTCString()
+               }
+        },
+        chet: { 
+            upload:[
+                {
+                    src: 'pages/index.html',
+                    dest: 'index.html'
+                },
+                {
+                    src: 'src/*.js',
+                    dest: 'js/'
+                },
+                {
+                    src: 'styles/css/*.css',
+                    dest: 'styles/'
+                },
+                {
+                    src: 'lib/flipclock/*.js',
+                    dest: 'js/flipclock/'
+                }
+            ]
+        }
+    },
+    sass: {
+        dist: { 
+            files: {
+                'styles/css/hfw.css': 'styles/sass/hfw.scss'
+            }
+        }
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      },
+      sass: { 
+        files: ['styles/sass/*.scss', 'pages/index.html', 'src/*.js'],
+        tasks: ['sass:dist', 's3']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  /*
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  */
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-s3');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
